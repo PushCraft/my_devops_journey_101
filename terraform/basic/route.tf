35e99765470206285d264d7eb98031f6c8d1117f
PushCraft <consultant.jaskaran@gmail.com>
2026-03-30 21:57:14 -0400

    newd

--- terraform/basic/route.tf
+++ terraform/basic/route.tf
@@ -1,7 +1,7 @@
 resource "aws_route_table" "DemobasicPublic_RTB" {
   vpc_id = aws_vpc.basicvpc.id
   tags = {
-    "source" = var.source-tag
+    varargument-key1 = var.source-tag
   }
   route {
     gateway_id = aws_internet_gateway.basic_igw.id
