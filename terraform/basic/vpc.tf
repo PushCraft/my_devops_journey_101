49b4089faf12263a68cf2cce6a191ecf6dbb64b3
PushCraft <consultant.jaskaran@gmail.com>
2026-03-30 22:18:49 -0400

    Ds

--- terraform/basic/vpc.tf
+++ terraform/basic/vpc.tf
@@ -3,6 +3,6 @@ resource "aws_vpc" "basicvpc" {
   cidr_block           = var.vpc_cidr # 65K IP's
   enable_dns_hostnames = true
   tags = {
-    var.argument-key1 = var.source-tag
+    var.argument_key1 = var.source-tag
   }
 }
