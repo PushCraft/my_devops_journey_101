b8647530ee1bff1f26260fd96d0b6771d94547c6
PushCraft <consultant.jaskaran@gmail.com>
2026-04-05 05:34:17 -0400

    s3 backend set up

--- terraform/basic/provider.tf
+++ terraform/basic/provider.tf
@@ -1,6 +1,13 @@
 provider "aws" {
   region = var.region
 }
+terraform {
+  backend "s3" {
+    bucket         = "polaris-state-store-bucketxyz"
+    key            = "polaris-basic.tfstate"
+    region         = "ca-central-1"
+  }
+}
 
 
 
