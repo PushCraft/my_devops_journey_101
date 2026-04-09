791b38c8b438f2cb1243995861e9633a7541cbbb
PushCraft <consultant.jaskaran@gmail.com>
2026-04-08 21:30:43 -0400

    new changes

---
+++ terraform/basic_2_functions/infra.tf
@@ -0,0 +1,29 @@
+
+# VPC
+resource "aws_vpc" "DemoBasicVPC" {
+  cidr_block           = var.vpcCidr
+  enable_dns_hostnames = true
+  tags = {
+    "source" = var.tag1
+  }
+}
+
+# IGW
+resource "aws_internet_gateway" "DemoBasicIGW" {
+   vpc_id = aws_vpc.DemoBasicVPC
+   tags = {
+      "source" = var.tag1
+   }
+}
+
+
+
+# Subnets ------------
+
+resource "aws_subnet" "subnet-1" {
+   vpc_id = var.vpcCidr
+   
+   cidr_block = var.cidrSubnet1
+   availability_zone  =  var.az1
+   
+}
---
+++ terraform/basic_2_functions/vars.tf
@@ -0,0 +1,7 @@
+variable "region" {}
+variable "vpcCidr" {}
+variable "tag1" {}
+
+variable "cidrSubnet1" {}
+
+variable "az1" {}
