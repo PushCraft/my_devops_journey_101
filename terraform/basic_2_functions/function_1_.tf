42eb96002c7797da86ef2bd51bc832fa1566bb9d
PushCraft <consultant.jaskaran@gmail.com>
2026-04-27 00:20:02 -0400

    New changes

--- terraform/basic_2_functions/function_1_.tf
+++ terraform/basic_2_functions/function_1_.tf
@@ -5,7 +5,7 @@ resource "aws_subnet" "PublicSubnet_functions" {
   vpc_id = aws_vpc.DemoBasicVpc.id
   #count  = 3
   # This will create '3' specified resources, Which are 3 subnets.  
-    #Indexed would be "0", "1" and "2". 
+  #Indexed would be "0", "1" and "2". 
   count = length(var.publicSubnetCidr)
   # The count value will be adjusted depending upon the number of "CIDR" list items you provide.
   # You will provide the 3 CIDR blocks as 'list' type. When assigning to variable value.
@@ -17,7 +17,7 @@ resource "aws_subnet" "PublicSubnet_functions" {
 
 
   # Since you have 3 subnet's. You will also provide the 3 cidr block in list type. 
-  map_public_ip_on_launch = true 
+  map_public_ip_on_launch = true
   # This will assign an Public IP for every resource in this subnet. 
   tags = {
     "source" = var.tagX
--- terraform/basic_2_functions/output.tf
+++ terraform/basic_2_functions/output.tf
@@ -1,13 +1,13 @@
 #Give me the IDs of ALL subnets created
 output "publicsubnetids-Splat" {
-   value = aws_subnet.PublicSubnet_functions.*.id
-    # "*" ==> Index value. Also called "Splat Expression"
-    # "Splat expression" = > Is used to extract attribute from all instances of 
-            # a resource creaeting using 'count' or 'for_each'. 
+  value = aws_subnet.PublicSubnet_functions.*.id
+  # "*" ==> Index value. Also called "Splat Expression"
+  # "Splat expression" = > Is used to extract attribute from all instances of 
+  # a resource creaeting using 'count' or 'for_each'. 
 }
 
 # Manually extracting the ID's. Index by index.
- #Give me the ID of subnet at index 'X'.
+#Give me the ID of subnet at index 'X'.
 output "PublicSubnet-0" {
   value = aws_subnet.PublicSubnet_functions.0.id
 }
