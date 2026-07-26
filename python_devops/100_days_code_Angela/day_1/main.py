26a8b59620677e5643aab7bc71b99545273e03fb
PushCraft <consultant.jaskaran@gmail.com>
2026-07-26 00:31:12 -0400

    day1#

--- python_devops/100_days_code_Angela/day_1/main.py
+++ python_devops/100_days_code_Angela/day_1/main.py
@@ -1,1 +1,12 @@
-print("Echo Hello World!")
+# Greeting
+welcome = "Welcome to the Band Name Generator."
+print(welcome)
+
+# Ask the user for the city that they grew up in. 
+city = input("What's the name of the city you grew up in?\n").capitalize()
+
+# Ask the user for the last thing they ate.
+food = input("What's the last thing you ate?\n").capitalize()
+
+# Combine the name of the city and food they ate and show the band name.
+print("Your band name could be " + city + " " + food)
